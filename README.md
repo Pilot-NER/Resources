527232240aa789e6565edd1164f35e9f8fd9d9f7
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-03-29 14:56:53 -0700

    Update (with Algorithm 9)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Resources
 
-The `sample memos.py` has imported the strings of memos and vendors from the `Sample memos - memos.csv`. 
+The `sample memos.py` has imported the strings of memos and vendors from the `Sample memos - memos.csv`.
 
 * `memos_list` : a list of strings of memos
 * `vendors_list`: a list of vendor names
@@ -12,3 +12,8 @@ To use this Python file:
 1. Clone this entire Resources folder using `git clone https://github.com/Final-Project-Freshman/Resources.git`
 2. Put the codes in the file `sample memos.py` and calls the lists and/or dictionary.
 Voila!
+
+
+---
+Documents:
+1. [Rough Algorithm Ideas](https://docs.google.com/document/d/1TFI-G3dea151vb88T99FPon3gq1m1g6M4FKyL8wN-Jw/edit)
--- yong.py
+++ yong.py
@@ -23,16 +23,52 @@ with open("Sample memos - memos.csv", 'r') as memofile:
 
 # functions to analyze the pattern
 
-def remove_date(memos_list, vendors_list, memo_to_vendor_dict):
+def remove_date_ref_Crd(memos_list, vendors_list, memo_to_vendor_dict):
 	# remove the date from the string (assume format - "MM/DD")
+	
 	date = "[^\s]*\d\d/\d\d[^\s]*"
+	ref = "(?i)ref[\d^\s]*"
+	crd = "(?i)crd[\d^\s]*"
+	# num = "[^\s]*\d\d\d\d+[^\s]*"
+
 	new_list = []
 	for i, memo in enumerate(memos_list):
 		tmp = re.split(date, memo)
 		tmp = [x.lstrip().rstrip() for x in tmp]
-		new_list.append(' '.join(tmp).lstrip().rstrip())
+		tmp = ' '.join(tmp).lstrip().rstrip()
+
+		tmp = re.split(ref, tmp)
+		tmp = [x.lstrip().rstrip() for x in tmp]
+		tmp = ' '.join(tmp).lstrip().rstrip()
+
+		tmp = re.split(crd, tmp)
+		tmp = [x.lstrip().rstrip() for x in tmp]
+		tmp = ' '.join(tmp).lstrip().rstrip()
+
+		new_list.append(tmp)
 
 	return new_list
 
-memos_list_wo_date = remove_date(memos_list, vendors_list, memo_to_vendor_dict)
-pprint.pprint(memos_list_wo_date)
+memos_list_wo_date = remove_date_ref_Crd(memos_list, vendors_list, memo_to_vendor_dict)
+
+
+# algorithm 9
+def before_keywords(memos_list):
+	keywords = ["(?i)\sinc.\s", "(?i)\sLLC\s", "(?i)\sCO\s", "(?i)\sLimited\s", "(?i)\sINC\s", "(?i)\sCorporation\s", "(?i)\s.com\s", "(?i)\s.net\s"]
+	ans = dict()
+	for m in memos_list:
+		for k in keywords:
+			tmp = re.split(k, m)
+			if len(tmp) > 1 and (m not in ans or (m in ans and len(tmp[0]) < len(ans[m]))):
+				ans[m] = tmp[0] + " " + k[6:-2]
+	return ans
+
+alg_9 = before_keywords(memos_list_wo_date)
+for m, k in alg_9.items():
+	print(m, ":", k)
+
+
+
+
+
+
